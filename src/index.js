59edbc912e6b5fabf48d8b1b0e1ed369fc418375
Takuya Obara <tobara@schoo.jp>
2017-12-14 17:42:24 +0900

    implement "TestApp" component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log('percel-test');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class TestApp extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="app">Hello, world! I am a {this.constructor.name}.</div>
+    );
+  }
+};
+
+ReactDOM.render(
+  <TestApp></TestApp>,
+  document.getElementById('app')
+);
